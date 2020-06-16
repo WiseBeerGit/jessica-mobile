53cd53591b7010f9bee0cedc4ed0a857d829f3d0
invisikami <18693838+invisikami@users.noreply.github.com>
2020-06-16 16:26:32 -0700

    rev

--- script.js
+++ script.js
@@ -4,3 +4,44 @@ let vh = window.innerHeight * 0.01;
 document.documentElement.style.setProperty('--vh', `${vh}px`);
 
 // source: https://css-tricks.com/the-trick-to-viewport-units-on-mobile/
+
+function setBGImg() {
+    var fileName = location.href.split("/").slice(-1)[0];
+    let layer0 = document.getElementById("layer0");
+    console.log(fileName);
+
+    switch (fileName) {
+        case "index.html":
+            layer0.style.background = "linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('images/bg-img.jpg') no-repeat center center fixed";
+            layer0.style.backgroundSize = "cover";
+            console.log("switch on index called");
+            break;
+        case "services.html":
+            layer0.style.background = 'linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url("images/services.jpg") no-repeat center center fixed';
+            layer0.style.backgroundSize = "cover";
+            console.log("switch on index called");
+            break;
+        case "book.html":
+            layer0.style.background = 'linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url("images/agenda.jpg") no-repeat center center fixed';
+            layer0.style.backgroundSize = "cover";
+            console.log("switch on index called");
+            break;
+        case "location.html":
+            layer0.style.background = 'linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url("images/map.jpg") no-repeat center center fixed';
+            layer0.style.backgroundSize = "cover";
+            console.log("switch on index called");
+            break;
+        case "about.html":
+            layer0.style.background = 'linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url("images/about.jpg") no-repeat center center fixed';
+            layer0.style.backgroundSize = "cover";
+            console.log("switch on index called");
+            break;
+
+        default:
+            layer0.style.background = '#000';
+    }
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+    setBGImg();
+});
